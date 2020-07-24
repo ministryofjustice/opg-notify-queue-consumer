66113b97b798c05228c24de8a021bb3b5d473f5c
Gurpreet Singh Mannan <gurpreet.singh@digital.justice.gov.uk>
2020-07-24 16:55:04 +0100

    SW-2485 Guzzle Response completed with unit tests and formatting fixes

--- src/Opg/Handler/SendToNotifyHandler.php
+++ src/Opg/Handler/SendToNotifyHandler.php
@@ -11,6 +11,9 @@ use League\Flysystem\FileNotFoundException;
 use League\Flysystem\Filesystem;
 use Opg\Command\SendToNotify;
 use UnexpectedValueException;
+use InvalidArgumentException;
+
+use function PHPUnit\Framework\isNull;
 
 class SendToNotifyHandler
 {
@@ -45,9 +48,50 @@ class SendToNotifyHandler
         $response = $this->notifyClient->sendPrecompiledLetter(
             $command->getUuid(),
             $contents
-            );
+        );
 
-        $guzzleResponse = $this->guzzleClient->put('/api/public/v1/correspondence/update-send-status');
+        /*
+         * The response received from notify is in the following format
+         * {
+              "id": "740e5834-3a29-46b4-9a6f-16142fde533a", //the notify id
+              "reference": "your-letter-reference", // the uuid for the correspondence
+              "postage": "postage-you-have-set-or-None" // the type of postage you selected
+            }
+         *
+         * */
+
+        /*
+         * Once we have that notify id, we can check for the status of our correspondence
+         * by using the getNotification method on the notify client which takes in the notify
+         * id as an argument. The response provides a 200 and a raft of data which may be useful
+         * but we are only concerned with the status */
+        $statusQuery = $this->notifyClient->getNotification($response['id']);
+
+        //TODO the statusQuery returns Null if the notification cannot be found
+//        if (isNull($statusQuery)) {
+//            throw new InvalidArgumentException("No notification found for the ID");
+//        }
+
+        $correspondenceStatus = $statusQuery['status'];
+
+        /*
+         * TODO the status from the notification response states the status is either sending, delivered,
+         * permanent-failure, temporary-failure, technical failure - in the original endpoint ticket these were
+         * queued for sending, Sent for posting (the only one which should give a notify id), Posted and rejected.
+         * Are we meant to map these statuses to statuses from Notify and/or set these depending on the part of the process
+         * the correspondence has got to?*/
+
+        $payload = [
+            $command->getDocumentId(), //the document id in the database
+            $response['id'], //the notify id
+            $correspondenceStatus //the status of the correspondence
+        ];
+
+        $guzzleResponse = $this->guzzleClient->request(
+            'PUT',
+            '/api/public/v1/correspondence/update-send-status',
+            ['json' => $payload]
+        );
 
 
         // TODO make sure it handles unique but duplicate messages
--- tests/unit/OpgTest/Handler/SendToNotifyHandlerTest.php
+++ tests/unit/OpgTest/Handler/SendToNotifyHandlerTest.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace OpgTest\Handler;
 
-use _HumbugBox5f943a942674\Nette\FileNotFoundException;
-use _HumbugBox5f943a942674\Nette\Neon\Exception;
 use Alphagov\Notifications\Client;
+use GuzzleHttp\Client as GuzzleClient;
 use League\Flysystem\Filesystem;
 use Opg\Command\SendToNotify;
 use Opg\Handler\SendToNotifyHandler;
-use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -26,16 +24,24 @@ class SendToNotifyHandlerTest extends TestCase
      */
     private $mockNotifyClient;
 
+    /**
+     * @var GuzzleClient|MockObject
+     */
+    private $mockGuzzleClient;
+
     private SendToNotifyHandler $handler;
+
 
     public function setUp(): void
     {
         parent::setUp();
         $this->mockFilesystem = $this->createMock(Filesystem::class);
         $this->mockNotifyClient = $this->createMock(Client::class);
+        $this->mockGuzzleClient = $this->createMock(GuzzleClient::class);
         $this->handler = new SendToNotifyHandler(
             $this->mockFilesystem,
-            $this->mockNotifyClient
+            $this->mockNotifyClient,
+            $this->mockGuzzleClient
         );
     }
 
@@ -45,6 +51,7 @@ class SendToNotifyHandlerTest extends TestCase
             'id' => '123',
             'uuid' => 'asd-456',
             'filename' => 'document.pdf',
+            'documentId' => 456
         ];
 
         $contents = "abcdef";
@@ -53,6 +60,27 @@ class SendToNotifyHandlerTest extends TestCase
             "id" => "740e5834-3a29-46b4-9a6f-16142fde533a",
             "reference" => "unique_ref123",
             "postage" => "first"
+        ];
+
+        $statusQuery = [
+              "id" => "740e5834-3a29-46b4-9a6f-16142fde533a",
+              "reference" => "unique_ref123",
+              "line_1" => "742 Evergreen Terrace ",
+              "line_2" => "Springfield",
+              "postcode" => "S1M 2SO",
+              "type" => "letter",
+              "status" => "sending",
+              "body" => "",
+              "created_at" => "",
+              "created_by_name" => "",
+              "sent_at" => "",
+              "completed_at" => ""
+        ];
+
+        $payload = [
+            456,
+            "740e5834-3a29-46b4-9a6f-16142fde533a",
+            "sending"
         ];
 
         $command = SendToNotify::fromArray($data);
@@ -69,6 +97,17 @@ class SendToNotifyHandlerTest extends TestCase
             ->with($data['uuid'], $contents)
             ->willReturn($response);
 
+        $this->mockNotifyClient
+            ->expects(self::once())
+            ->method('getNotification')
+            ->with($response['id'])
+            ->willReturn($statusQuery);
+
+        $this->mockGuzzleClient
+            ->expects(self::once())
+            ->method('request')
+            ->with('PUT', '/api/public/v1/correspondence/update-send-status', ['json' => $payload]);
+
         $this->handler->handle($command);
 
     }
@@ -76,9 +115,10 @@ class SendToNotifyHandlerTest extends TestCase
     public function testEmptyPdfFailure(): void
     {
         $data = [
-            'id' => '123',
+            'id' => "123",
             'uuid' => 'asd-456',
             'filename' => 'document.pdf',
+            'documentId' => 456
         ];
 
         $command = SendToNotify::fromArray($data);
@@ -87,6 +127,65 @@ class SendToNotifyHandlerTest extends TestCase
 
         self::expectException(\UnexpectedValueException::class);
         self::expectExceptionMessage("Cannot read PDF");
+
+        $this->handler->handle($command);
+
+    }
+
+    public function testNotificationIdNotFound(): void
+    {
+        $data = [
+            'id' => '123',
+            'uuid' => 'asd-456',
+            'filename' => 'document.pdf',
+            'documentId' => 456
+        ];
+
+        $contents = "abcdef";
+
+        $response = [
+            "id" => "740e5834-3a29-46b4-9a6f-16142fde533a",
+            "reference" => "unique_ref123",
+            "postage" => "first"
+        ];
+
+        $statusQuery = [
+            "id" => "740e5834-3a29-46b4-9a6f-16142fde533a",
+            "reference" => "unique_ref123",
+            "line_1" => "742 Evergreen Terrace ",
+            "line_2" => "Springfield",
+            "postcode" => "S1M 2SO",
+            "type" => "letter",
+            "status" => "sending",
+            "body" => "",
+            "created_at" => "",
+            "created_by_name" => "",
+            "sent_at" => "",
+            "completed_at" => ""
+        ];
+
+        $command = SendToNotify::fromArray($data);
+
+        $this->mockFilesystem
+            ->expects(self::once())
+            ->method('read')
+            ->with($data['filename'])
+            ->willReturn($contents);
+
+        $this->mockNotifyClient
+            ->expects(self::once())
+            ->method('sendPrecompiledLetter')
+            ->with($data['uuid'], $contents)
+            ->willReturn($response);
+
+        $this->mockNotifyClient
+            ->expects(self::once())
+            ->method('getNotification')
+            ->with($response['id'])
+            ->willReturn($statusQuery); //TODO to test this with a NULL but the tests below don't pass
+
+//        self::expectException(InvalidArgumentException::class);
+//        self::expectExceptionMessage("No notification found for the ID");
 
         $this->handler->handle($command);
 
