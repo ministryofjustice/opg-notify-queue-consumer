2c6b43921c4922c854453b325c6232b1a095cab4
Andy Price <sixdaysandy@gmail.com>
2020-11-12 11:14:50 +0000

    SP-538 - Update localstack to latest version (0.12.2)

--- scripts/localstack/localstack_init.sh
+++ scripts/localstack/localstack_init.sh
@@ -1,2 +1,2 @@
 awslocal sqs create-queue --queue-name notify --attributes VisibilityTimeout=30,ReceiveMessageWaitTimeSeconds=0
-awslocal --endpoint-url=http://localhost:4572 s3 mb s3://localbucket
+awslocal --endpoint-url=http://localhost:4566 s3 mb s3://localbucket
